b27580d52a3ef61377478b647fb80806fc17757c
timbledum <timbledum@gmail.com>
2018-07-08 19:16:58 +1200

    Refactor out trademe scraping

---
+++ tmhouses.py
@@ -0,0 +1,70 @@
+"""Extract data from trademe."""
+import requests
+from bs4 import BeautifulSoup
+
+from settings import settings
+
+TM_SITE = r"https://www.trademe.co.nz"
+FIND_LINKS = {"class": "tmp-search-card-list-view__link"}
+
+
+def get_property_table(href):
+    """Find the table with the key property facts on a property page and return as dict."""
+    prop_details = requests.get(TM_SITE + href)
+    prop_soup = BeautifulSoup(prop_details.text, "html.parser")
+
+    listing_table = prop_soup.find(id="ListingAttributes")
+    listing_table_rows = [row for row in listing_table.contents if row != "\n"]
+
+    listing_dict = {}
+    for p in listing_table_rows:
+        key = p.contents[1].text.strip().replace(":", "")
+        value = p.contents[3].text.strip().replace("\xad", "")
+        listing_dict[key] = value
+
+    listing_date = prop_soup.find(id="PriceSummaryDetails_ListedStatusText")
+    listing_dict["Date listed"] = listing_date.text[8:18].replace(",", "") + " 18"
+
+    return listing_dict
+
+
+def get_href_id(prop):
+    """Helper function to extract key property information from trademe search site (url and ID)."""
+    output = {}
+    output["href"] = prop["href"]
+    output["id"] = prop["id"]
+    return output
+
+
+def get_properties(bs_tree, old_properties):
+    """Get the property urls from the trademe search screen and populate with underlying information."""
+    properties = bs_tree.find_all(**FIND_LINKS)
+    data = [get_href_id(prop) for prop in properties]
+    data_filtered = [prop for prop in data if prop["id"] not in old_properties]
+
+    for prop in data_filtered:
+        print("Processing:", prop["id"])
+        prop.update(get_property_table(prop["href"]))
+    return data_filtered
+
+
+def get_trademe_data(old_properties):
+    """Scan through the pages of the trademe search scraping the property data."""
+    tm_request = requests.get(settings.tm_url)
+    html = tm_request.text
+    html_bs = BeautifulSoup(html, "html.parser")
+
+    properties_data = get_properties(html_bs, old_properties)
+
+    next_links = html_bs.find(id="PagingFooter")("a")[:-1]
+    next_pages = [TM_SITE + a["href"] for a in next_links]
+
+    for number, page in enumerate(next_pages):
+        print("Processing page", number + 2)
+        page_request = requests.get(page)
+        page_html = page_request.text
+        page_bs = BeautifulSoup(page_html, "html.parser")
+        page_data = get_properties(page_bs, old_properties)
+        properties_data += page_data
+
+    return properties_data
