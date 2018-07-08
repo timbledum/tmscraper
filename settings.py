692123882ee2d4c0ca0571bbdfebb54ca5bd2b97
timbledum <timbledum@gmail.com>
2018-07-08 19:20:48 +1200

    Correct some path joining to make cross platform

--- settings.py
+++ settings.py
@@ -1,14 +1,15 @@
 """Get settings YAML file and load to object."""
+import os
 from collections import namedtuple
 from yaml import load
 
-full_path = "/Users/timbledum/Documents/Python/tmhouse/"
+FULL_PATH = "/Users/timbledum/Documents/Python/tmhouse/"
 
 try:
     with open("settings.yaml") as file:
         settings_yaml = load(file)
-except FileNotFoundError:
-    with open(full_path + "settings.yaml") as file:
+except FileNotFoundError: # If launched by automatic scheduler
+    with open(os.path.join(FULL_PATH, "settings.yaml")) as file:
         settings_yaml = load(file)
 
 settings = namedtuple("Settings", settings_yaml.keys())(*settings_yaml.values())
